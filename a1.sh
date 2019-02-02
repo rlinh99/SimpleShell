3d3f32233afc60573e8fc59dc1fb77876a605b89
Sen Lin <33269903+rlinh99@users.noreply.github.com>
2019-02-01 19:36:35 -0800

    Done- A1
    Finalized A1 submission

--- a1.sh
+++ a1.sh
@@ -15,7 +15,7 @@ done
 for i in $CURRENT_IP
 do
 	if [[ $i == *"192."* ]];then
-		CURRENT_INNER_IPv4="$i"
+		CURRENT_OUTER_IPv4="$i"
 	fi
 done
 
@@ -23,44 +23,44 @@ done
 HOSTS="January February March December Autumn Spring November August October Solstice Year May July Winter Summer Fall April June September Equinox Unreachable1 Unreachable2"
 NETWORKS="admin net16 net17 net18 net19"
 
-#ipv6 prefix
-Prefix="fdd0:8184:d967"
-
 #ipv6 server used to test is Autumn
-DEST_IPV6="fdd0:8184:d967:17:94e8:7f4e:5a36:6bfe/64 fdd0:8184:d967:8017:94e8:7f4e:5a36:6bfe/64 fdd0:8184:d967:17:250:56ff:fe85:6737 fdd0:8184:d967:8017:250:56ff:fe85:6737"
+DEST_IPV6="fdd0:8184:d967:17:ecc6:84e1:4920:8927 fdd0:8184:d967:8017:ecc6:84e1:4920:8927 fdd0:8184:d967:17:250:56ff:fe85:6737 fdd0:8184:d967:8017:250:56ff:fe85:6737"
 
 echo Reachability to destination using Hostname
 echo
-
+echo Check connection start with 192.
 for host in $HOSTS
 do	
 	#check connection of 192
-	ping -c 1 "$host" >/dev/null
+	ping -c 1 "$host" >/dev/null 2>&1
 	if [ $? -eq 0 ]; then
 		echo Host "$host" is Reachable from "$CURRENT" && tracepath "$host"
 		#extra ping to make sure arp is working
-		ping -c 1 "$host.$nw" >/dev/null && arp "$host"
+		echo Ethernet address of the connection
+		ping -c 1 "$host" >/dev/null && arp "$host"
 	else
-		echo Host "$host" is NOT Reachable from "$CURRENT"
+		echo Host "$host" is NOT Reachable from host "$CURRENT"
 	fi
 	echo
+	echo Check connection start with 172.
 	#check connection of 172
 	for nw in $NETWORKS
 	do
-		ping -c 1 "$host.$nw" >/dev/null
+		ping -c 1 "$host.$nw" >/dev/null 2>&1
 		if [ $? -eq 0 ]; then
-			echo Host "$host.$nw" is Reachable from "$CURRENT" && tracepath "$host" 
+			echo Host "$host.$nw" is Reachable from "$CURRENT" && tracepath "$host.$nw" 
 			#extra ping to make sure arp is working
-			ping -c 1 "$host.$nw" >/dev/null && arp "$host"
+			echo Ethernet address of the connection
+			ping -c 1 "$host.$nw" >/dev/null && arp "$host.$nw"
 		else
-			echo Host "$host.$nw" is NOT Reachable from "$CURRENT"
+			echo Host "$host.$nw" is NOT Reachable from host "$CURRENT"
 		fi
 		echo
 	done
 done
 
 echo Reachbility to destination using IPv4
-echo Check inner link (addresses start with 172.)
+echo Check inner link "(addresses start with 172.)"
 echo
 	#check connection of inner link
 for i in 16 17 18 19
@@ -69,36 +69,44 @@ do
 	do
 		ping -c 1 "172.$i.1.$j" >/dev/null
 		if [ $? -eq 0 ]; then
-			echo Host "172.$i.1.$j" is Reachable from "$CURRENT_INNER_IPv4" && tracepath "172.$i.1.$j" 
+			echo Host "172.$i.1.$j" is Reachable from host "$CURRENT_INNER_IPv4" && tracepath "172.$i.1.$j" 
 			#extra ping to make sure arp is working
-			ping -c 1 "$host.$nw" >/dev/null && arp "172.$i.1.$j"
+			echo Ethernet address of the connection
+			ping -c 1 "172.$i.1.$j" >/dev/null && arp "172.$i.1.$j"
 		else
 			echo Host "172.$i.1.$j" is NOT Reachable from "$CURRENT_INNER_IPv4"
 		fi
 		echo
 	done
-
 done
-echo
-echo Check outer link (addresses start with 192.)
+
+echo Reachbility to destination using IPv4
+echo Check outer link "(addresses start with 192.)"
 echo
 	#check connection of outer link
 for i in {1..25}
 do
-	ping -c 1 "192.168.0.$i"
+	ping -c 1 "192.168.0.$i" >/dev/null
 	if [ $? -eq 0 ]; then
-		echo Host "192.168.0.$i" is Reachable from "$CURRENT_OUTER_IPv4" && tracepath "192.168.0.$i" 
+		echo Host "192.168.0.$i" is Reachable from host "$CURRENT_OUTER_IPv4" && tracepath "192.168.0.$i" 
 		#extra ping to make sure arp is working
-		ping -c 1 "$host.$nw" >/dev/null && arp "192.168.0.$i"
+		echo Ethernet address of the connection
+		ping -c 1 "192.168.0.$i" >/dev/null && arp "192.168.0.$i"
 	else
 		echo Host "192.168.0.$i" is NOT Reachable from "$CURRENT_OUTER_IPv4"
 	fi
 	echo
 done
 
+#ipv6 server used to test is Autumn
 echo Reachbility to destination using IPv6
 echo
 for addr in $DEST_IPV6
 do 
-
+	ping6 -c 1 "$addr" >/dev/null
+	if [ $? -eq 0 ]; then
+		echo "$addr is reachable from current host" && tracepath6 "$addr"
+	else
+		echo "$addr is not reachable from current host"
+	fi
 done
